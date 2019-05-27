315149dd2be95e740476109cb0a2d2646e8ab358
Louis Shinohara <louishinohara@gmail.com>
2019-05-26 21:01:31 -0400

    Xcode

---
+++ RUsh/RUsh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RUsh
+//
+//  Created by Louis Shinohara on 5/26/19.
+//  Copyright © 2019 Louis Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
