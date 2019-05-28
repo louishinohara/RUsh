401be2fab021a01429b7442a9d313024b8f281ab
Louis Shinohara <louishinohara@gmail.com>
2019-05-28 15:16:47 -0400

    Adding google authentication

--- RUsh/RUsh/AppDelegate.swift
+++ RUsh/RUsh/AppDelegate.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
     var window: UIWindow?
-
+    let userDefault = UserDefaults()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
-
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {                              //For Google Authentication
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        } else {
+            guard let authentication = user.authentication else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+            
+            Auth.auth().signInAndRetrieveData(with: credential) { (result, error) in
+                if error == nil {
+                    self.userDefault.set(true, forKey: "usersignedin")
+                    self.userDefault.synchronize()
+                   // self.window.rootViewController.performSegue(withIdentifier: , sender: nil)
+                } else {
+                    print(error?.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {     // For Google Authentication
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: [:])
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- RUsh/RUsh/ViewController.swift
+++ RUsh/RUsh/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 import Firebase
-class ViewController: UIViewController {
+import GoogleSignIn
+class ViewController: UIViewController, GIDSignInUIDelegate  {
+    
 
     
     @IBOutlet weak var otpTcx: UITextField!
     @IBOutlet weak var phoneOu: UITextField!
     
+    let userDefault = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        hideKeyboardWhenTappedAround()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        //hideKeyboardWhenTappedAround USED FOR PHONE NUMBER AUTHENTICATION. DOESNT WORK WITH GOOGLE
+        
     }
 
     @IBAction func phoneSignIn(_ sender: Any) {
@@ -26,15 +32,39 @@ class ViewController: UIViewController {
         PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) {(verificationId,error) in
             if error == nil {
                 print(verificationId)
+                guard let verifyId = verificationId else { return }
+                self.userDefault.set(verifyId,forKey:"verificationId")
+                self.userDefault.synchronize()
             } else {
                 print("Unable to get Secret Verification Code from firebase",error?.localizedDescription)
             }
     }
     }
     
-    
-    @IBAction func verifyOtp(_ sender: Any) {
+    @IBAction func verifyOtpPressed(_ sender: Any) {
+        
+        guard let otpCode = otpTcx.text else { return }
+        
+        guard let verificationId = userDefault.string(forKey: "verificationId") else { return }
+        
+        let credential = PhoneAuthProvider.provider().credential(withVerificationID: verificationId, verificationCode: otpCode)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) {(success, error) in
+            if error == nil {
+                print(success)
+                print("User Signed in...")
+                
+            } else {
+                print("Something went wrong... \(error?.localizedDescription)")
+            }
+        }
+        
     }
-    
 }
+    
 
+
+// Gotta figure out how to fix tap around in viewdidload to work with google sign in
+// Need a segue for log out button.
+// Need to add the thing which enables users to sign out and sign in with a different email
+// Stay signed in after inputting
